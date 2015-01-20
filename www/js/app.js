1211e0ef0630b6e482127879696d3cf8fb65a3d7
Nairobi Albarran <nairobi.albarran.contractor@bbva.com>
2015-01-20 17:54:31 +0100

    add autodiscover property

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,55 @@
-var app = angular.module("demoapp", ['leaflet-directive']);
+  var app = angular.module("demoapp", ['leaflet-directive']);
 
-app.controller("DemoController", ["$scope", function ($scope) {
-        angular.extend($scope, {
-            center: {
-                lat: 51.505,
-                lng: -0.09,
-                zoom: 16,
-                autoDiscover: true
-            },
-            markers: {
-                lat: 51.505,
-                lng: -0.09,
-                zoom: 16}
+  app.controller("DemoController", [ "$scope", "$location", function($scope, $location, $http) {
+
+  angular.extend($scope,{
+      center: {
+                lat: 40.4878127,
+                lng: -3.6673434,
+                autoDiscover: true,
+                zoom: 16
+              },
+      markers: {
+                myMarker: {
+                    lat: 40.4878127,
+                    lng: -3.6673434,
+                    message: "Hello",
+                    focus: true,
+                    draggable: false
+                    },
+                otherMarker:{
+                  lat: 40.4874738,
+                  lng: -3.6637454,
+                  message: "Hello",
+                  focus: true,
+                  draggable: false
+                   }
+               },
+        link: function(scope, element, attrs) {
+        var points = [{lat: 40.4878127, lng: -3.6673434},{lat: 40.4874738, lng: -3.6637454}];
+              updatePoints(points);
+
+              function updatePoints(pts) {
+                 for (var p in pts) {
+                    L.marker([pts[p].lat, pts[p].lng]).addTo(map);
+                 }
+              }
+              scope.$watch(attr.pointsource, function(value) {
+                 updatePoints(value);
+              });
+           }
+
+     });
+
+     $scope.$on("centerUrlHash", function(event, centerHash) {
+                  console.log("url", centerHash);
+                  $location.search({ c: centerHash });
+                  console.log("entro");
         });
-    }]);
+    $scope.pointsFromController = [{lat: 40.4878127, lng: -3.6673434},{lat: 40.4874738, lng: -3.6637454}];
+     $scope.getPointsFromSomewhere = function() {
+       $http.get('wifi.json').success(function(somepoints) {
+       $scope.pointsFromController = somepoints;
+       });
+  };
+  }]);

5fb57828c05d541c4e36bc5dc34069c6ec78d774
Nairobi Albarran <nairobi.albarran.contractor@bbva.com>
2015-01-20 16:40:45 +0100

    [FEAT] add autodiscover property

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,77 @@
+// Ionic Starter App
+
 var app = angular.module("demoapp", ['leaflet-directive']);
 
-app.controller("DemoController", ["$scope", function ($scope) {
-        angular.extend($scope, {
-            center: {
-                lat: 51.505,
-                lng: -0.09,
-                zoom: 16,
-                autoDiscover: true
-            },
-            markers: {
-                lat: 51.505,
-                lng: -0.09,
-                zoom: 16}
-        });
-    }]);
+app.controller("DemoController", [ "$scope", "$location", function($scope, $location) {
+
+   //here's the property you can just update.
+  /* $scope.pointsFromController = [{lat: 40.4878127, lng: -3.6673434},{lat: 40.4874738, lng: -3.6637454}];
+
+   //here's some contrived controller method to demo updating the property.
+   $scope.getPointsFromSomewhere = function() {
+     $http.get('http://miquelcamps.cartodb.com/api/v2/sql?q=SELECT * FROM public.wifis_p_blicas_de_madrid').success(function(somepoints) {
+         $scope.pointsFromController = somepoints;
+     });
+   }*/
+// Nothing here!
+angular.extend($scope, {
+
+    center: {
+              lat: 40.4878127,
+              lng: -3.6673434,
+              autoDiscover: true,
+              zoom: 16
+           },
+    markers: {
+              myMarker: {
+                lat: 40.4878127,
+                lng: -3.6673434,
+                message: "Hello",
+                focus: true,
+                draggable: false
+              },
+              otherMarker:{
+                lat: 40.4874738,
+                lng: -3.6637454,
+                message: "Hello",
+                focus: true,
+                draggable: false
+              }
+           }
+   });
+
+   $scope.$on("centerUrlHash", function(event, centerHash) {
+                console.log("url", centerHash);
+                $location.search({ c: centerHash });
+            });
+}]);
+
+
+
+
+           
+
+/*function MapCtrl($scope, $http) {
+   //here's the property you can just update.
+   $scope.pointsFromController = [{lat: 40, lng: -86},{lat: 40.1, lng: -86.2}];
+
+   //here's some contrived controller method to demo updating the property.
+   $scope.getPointsFromSomewhere = function() {
+     $http.get('http://miquelcamps.cartodb.com/api/v2/sql?q=SELECT * FROM public.wifis_p_blicas_de_madrid').success(function(somepoints) {
+         $scope.pointsFromController = somepoints;
+     });
+   }
+}*/
+
+/*$scope.markers = new Array();
+
+            $scope.$on("leafletDirectiveMap.click", function(event, args){
+                var leafEvent = args.leafletEvent;
+
+                $scope.markers.push({
+                    lat: leafEvent.latlng.lat,
+                    lng: leafEvent.latlng.lng,
+                    message: "My Added Marker"
+                });
+            });*/
+ //       } ]);
